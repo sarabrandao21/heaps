ef9211510ea601fd4336dd15e53f36df283006fc
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-09-29 20:11:47 -0700

    min heap

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -17,7 +17,11 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    # if odd is being added to the right
+    # if even is being added to the left 
+    @store << HeapNode.new(key, value)
+
+    heap_up(@store.length - 1)
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +29,17 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    if @store.empty? 
+      return nil 
+    end 
+
+    swap(0, @store.length - 1)
+    result = @store.pop
+
+    heap_down(0) unless @store.empty? 
+    return result.value
+      
+
   end
 
 
@@ -47,7 +61,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.empty?
   end
 
   private
@@ -57,15 +71,36 @@ class MinHeap
   #  It could be **very** helpful for the add method.
   # Time complexity: ?
   # Space complexity: ?
-  def heap_up(index)
-    
+  def heap_up(index) # last index first time 
+    parent = (index - 1) / 2
+    bottom = @store[index]
+
+    if index > 0 && @store[parent].key > bottom.key 
+      swap(index, parent)
+      heap_up(parent)
+    end 
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    
+    max_length = @store.length - 1 
+    child_left = index * 2 + 1
+    child_right = index * 2 + 2
+    top = @store[index]
+    
+    if index <= max_length 
+      if !@store[child_left].nil? && top.key > @store[child_left].key
+        heap_down(child_left)
+        swap(index, child_left)
+        
+      elsif !@store[child_right].nil? && top.key > @store[child_right].key 
+        swap(index, child_right)
+        heap_down(child_left)
+      end 
+    end 
   end
 
   # If you want a swap method... you're welcome
